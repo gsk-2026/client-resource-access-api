9f9797e12163b0581b5b6102b4bd60105828da00
“Sankui <gousankui@yahoo.com>
2026-07-19 16:16:31 -0500

    add test for SecurityConfig

--- client-resource-access-api/src/test/java/com/dreamtech/clientresourceaccessapi/config/SecurityConfigTest.java
+++ client-resource-access-api/src/test/java/com/dreamtech/clientresourceaccessapi/config/SecurityConfigTest.java
@@ -1,36 +1,19 @@
 package com.dreamtech.clientresourceaccessapi.config;
-/*
+
 import io.swagger.v3.oas.models.OpenAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
-*/
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-/*
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-*/
-import org.springframework.security.web.SecurityFilterChain;
 
-//import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@Configuration
+
 public class SecurityConfigTest {
-    /*
+
     private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
             .withUserConfiguration(SecurityConfig.class);
-    */
 
-    @Bean
-    public SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // Disables CSRF block
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Allows all endpoints
-        return http.build();
-    }
-
-    /*
     @Test
     void shouldRegisterPasswordEncoderBeanAsBCrypt() {
         contextRunner.run(context -> {
@@ -60,5 +43,5 @@ public class SecurityConfigTest {
             assertThat(openAPI.getInfo().getDescription()).isEqualTo("API documentation for managing resource access paths.");
         });
     }
-    */
+
 }
